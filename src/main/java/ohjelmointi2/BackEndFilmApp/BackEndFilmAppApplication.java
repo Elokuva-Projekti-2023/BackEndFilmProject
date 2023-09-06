3af5208873bade9a75b48961b0734301038b7e20
Arina <arina.soldan@myy.haaga-helia.fi>
2023-09-06 14:53:28 +0300

    Project starts

---
+++ src/main/java/ohjelmointi2/BackEndFilmApp/BackEndFilmAppApplication.java
@@ -0,0 +1,13 @@
+package ohjelmointi2.BackEndFilmApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndFilmAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndFilmAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ohjelmointi2/BackEndFilmApp/BackEndFilmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ohjelmointi2.BackEndFilmApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndFilmAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

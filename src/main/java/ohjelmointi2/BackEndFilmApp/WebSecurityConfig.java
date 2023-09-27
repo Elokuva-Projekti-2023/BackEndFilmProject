d0d4d29534fbedd25ed8bcc58574ae4dd8f69b44
Susanna <susanna.tuomi@myy.haaga-helia.fi>
2023-09-27 12:51:11 +0300

    Added WebSecurityConfig, UserDetailsServiceImpl and authorities, password_encoding, changes also in User class and repository

---
+++ src/main/java/ohjelmointi2/BackEndFilmApp/WebSecurityConfig.java
@@ -0,0 +1,75 @@
+package ohjelmointi2.BackEndFilmApp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import ohjelmointi2.BackEndFilmApp.web.UserDetailsServiceImpl;
+
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebSecurityConfig {
+	
+	@Autowired
+    private UserDetailsServiceImpl userDetailsService;	
+	
+    @Bean
+    public SecurityFilterChain filterchain(HttpSecurity http) throws Exception {
+      http
+      .cors()
+      .and()  
+      .authorizeHttpRequests((authorize) -> authorize
+		          .anyRequest().authenticated()
+		      )
+		      .formLogin()
+		          .defaultSuccessUrl("/", true)
+		          .permitAll()
+		          .and()
+		      .logout()
+		          .permitAll()
+		          .and()
+		      .httpBasic()
+		      .and()
+		      .csrf().disable();
+	      return http.build();
+    }
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+    
+    @Bean
+	CorsConfigurationSource corsConfigurationSource()
+	{
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("https://example.com"));
+		configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+}
--- src/main/java/ohjelmointi2/BackEndFilmApp/domain/User.java
+++ src/main/java/ohjelmointi2/BackEndFilmApp/domain/User.java
@@ -3,7 +3,11 @@ package ohjelmointi2.BackEndFilmApp.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,21 +15,30 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
 
 @Entity
 @Table(name = "users")
 public class User {
+	
+	public static final PasswordEncoder PASSWORD_ENCODER = new BCryptPasswordEncoder();
 
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long user_id;
 	
-	private String user_name;
+	@Column(name = "user_name")
+	private String userName;
 	
+	@Column(name = "user_email")
 	private String user_email;
 	
+	@Column(name="user_password")
 	private String user_password;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<MovieList> movieLists = new ArrayList<>();
     
@@ -34,13 +47,37 @@ public class User {
 		super();
 	}
     
-    
+    /*
 	public User(String user_name, String user_email, String user_password) {
 		super();
-		this.user_name = user_name;
+		this.userName = userName;
 		this.user_email = user_email;
 		this.user_password = user_password;
 	}
+
+	*/
+	
+
+	public User(String userName, String user_email, String user_password) {
+		super();
+		this.userName = userName;
+		this.user_email = user_email;
+		setUser_password(user_password);
+	}
+
+
+	public User(String userName, String user_password) {
+		super();
+		this.userName = userName;
+		this.user_password = user_password;
+	}
+
+
+	/*public User(String user_name, String user_password) {
+		super();
+		this.user_name = user_name;
+		this.user_password = user_password;
+	}*/
 
 
 	// getters and setters 
@@ -51,24 +88,43 @@ public class User {
 	public void setUser_id(Long user_id) {
 		this.user_id = user_id;
 	}
+	/*
 	public String getUser_name() {
 		return user_name;
 	}
 	public void setUser_name(String user_name) {
 		this.user_name = user_name;
-	}
+	}*/
+	
 	public String getUser_email() {
 		return user_email;
 	}
+	
+	public String getUserName() {
+		return userName;
+	}
+
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
 	public void setUser_email(String user_email) {
 		this.user_email = user_email;
 	}
 	public String getUser_password() {
 		return user_password;
 	}
+	/*
 	public void setUser_password(String user_password) {
 		this.user_password = user_password;
-	}	
+	}*/
+		
+		public void setUser_password(String user_password) {
+			this.user_password = PASSWORD_ENCODER.encode(user_password);
+		}
+		
 	
 	public void setMovieLists(List<MovieList> movieLists) {
 		this.movieLists = movieLists;
@@ -79,14 +135,23 @@ public class User {
 	    return movieLists;
 	}
 
+
+	@Override
+	public String toString() {
+		return "User [user_id=" + user_id + ", userName=" + userName + ", user_email=" + user_email + ", user_password="
+				+ user_password + "]";
+	}
+
 	
 	//ToString
-	
+	/*
 	@Override
 	public String toString() {
 		return "User [user_id=" + user_id + ", user_name=" + user_name + ", user_email=" + user_email
 				+ ", user_password=" + user_password + "]";
-	}
+	}*/
+	
+	
 	
     
     

81ecfb5266652e57ac92281446ee590f5729f987
olgatyynela <olga.lumiaho@gmail.com>
2023-10-30 14:21:28 +0200

    UserMovie entity, Service ja Controller

---
+++ src/main/java/ohjelmointi2/BackEndFilmApp/domain/UserMovieRepository.java
@@ -0,0 +1,7 @@
+package ohjelmointi2.BackEndFilmApp.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserMovieRepository extends CrudRepository<UserMovie, Long>{
+
+}
